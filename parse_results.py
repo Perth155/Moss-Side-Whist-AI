aebfea75fbcd18ce46cf31c7be032cb9916385e9
Robbie <fernandezrobert97@gmail.com>
2017-10-26 11:17:42 +0800

    Added latest parse_results.py

--- parse_results.py
+++ parse_results.py
@@ -2,21 +2,38 @@
 import sys
 import subprocess
 
-# Any agent whose name isn't in this list will be parsed as "Others"
-KNOWN_NAMES = ["My Agent", "ALL H1GH by abraram", "GR33D by abraram", "Pr0Bz by abraram", "G0PT by abraram", "G33D_EX by abraram"]
+TARGET_TIME = 200
+
+def check_time(output):
+	worst_time = 0
+	for line in output.split('\n'):
+		if line.startswith('Execution Time'):
+			time = float(line.split(':')[1])
+			if time > worst_time:
+				worst_time = time
+
+	if worst_time > TARGET_TIME:
+		raise Exception("{} target exceeded. Turn took {}".format(TARGET_TIME, worst_time))
 
 def parse_results(count):
 	wins_map = {}
 
 	for i in range(0, count):
 		results_map = {}
-		output = subprocess.check_output('java -jar Moss-Side-Whist-AI.jar', shell=True)
+		output = subprocess.check_output('java -cp out MossSideWhist', shell=True)
+		try:
+			check_time(output)
+		except Exception as e:
+			print(repr(e))
+			print_stats(wins_map, i)
+			return
+
 		results = output.split('\n')[-4:-1]
 		for line in results:
 			split_line = line.split(':')
-			agentname = split_line[0] if split_line[0] in KNOWN_NAMES else "Others"
+			agentname = split_line[0] if not split_line[0].startswith('RND') else "Random"
 			agentscore = split_line[1]
-			results_map[agentname] = agentscore
+			results_map[agentname] = int(agentscore)
 		best_score = None
 		best_agent = None
 		for agent in results_map:
@@ -31,6 +48,9 @@ def parse_results(count):
 		)
 		wins_map[best_agent] = wins_map.get(best_agent, 0) + 1
 
+	print_stats(wins_map, count)
+
+def print_stats(wins_map, count):
 	for name in wins_map:
 		print "{} won {} / {} games ({}%)".format(
 			name,
@@ -38,7 +58,7 @@ def parse_results(count):
 			count,
 			float(wins_map[name])/float(count) * 100
 		)
-
+			
 
 if __name__ == "__main__":
 	try:
